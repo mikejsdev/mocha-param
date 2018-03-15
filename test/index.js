4dc089bb563e1e576776a1e3d7d6514e0330ed71
mikejsdev <mikejsdev@gmail.com>
2018-03-15 22:53:10 +0000

    Add more tests

--- test/index.js
+++ test/index.js
@@ -1,9 +1,21 @@
-var itParam = require('../src/index').itParam;
+var itParam = require('../lib/index').itParam;
 var expect = require('chai').expect;
 
 
 describe("Calling itParam in a loop sync with a value", function () {
     itParam("should all pass", [1, 1, 1], function (value) {
+        expect(value).to.equal(1);
+    });
+});
+
+describe("Calling itParam with objects works", function () {
+    itParam("should all pass", [{ data: 1 }, { data: 1 }, { data: 1 }], function (value) {
+        expect(value.data).to.equal(1);
+    });
+});
+
+describe("Values are printed in description", function () {
+    itParam("value is: ${value}", [1, 1, 1], function (value) {
         expect(value).to.equal(1);
     });
 });
