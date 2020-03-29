cdd42a848d924344de1dec5c192af333e5bddbdc
HoldYourWaffle <ravivanrooijen@live.nl>
2020-03-30 00:53:53 +0200

    Add bound-this type to type definitions

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,4 +1,4 @@
-import { Done } from 'mocha'
+import { Context, Done } from 'mocha'
 
-export default function itParam<T>(desc: string, data: T[], callback: (value: T) => void): void;
-export default function itParam<T>(desc: string, data: T[], callback: (done: Done, value: T) => void): void;
+export default function itParam<T>(desc: string, data: T[], callback: (this: Context, value: T) => void): void;
+export default function itParam<T>(desc: string, data: T[], callback: (this: Context, done: Done, value: T) => void): void;
