ba869d5805962d19a3b20611f432a20f758a1fba
HoldYourWaffle <ravivanrooijen@live.nl>
2020-03-29 21:17:42 +0200

    Fix overload inference in type definitions

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,4 +1,4 @@
 import { Done } from 'mocha'
 
-export default function itParam<T>(desc: string, data: T[], callback: (value: T) => void): void;
 export default function itParam<T>(desc: string, data: T[], callback: (done: Done, value: T) => void): void;
+export default function itParam<T>(desc: string, data: T[], callback: (value: T) => void): void;
