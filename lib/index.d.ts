0ad3aa791fffef51e5667ded0f04423867ebe8d8
HoldYourWaffle <ravivanrooijen@live.nl>
2020-03-30 14:18:22 +0200

    Automatic utility definitions using mapped types

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,14 +1,11 @@
-import { Context, Done } from 'mocha'
+import { Context, Done, TestFunction } from 'mocha'
 
 
 type ItParamTestFuncSync = <T>(desc: string, data: T[], callback: (this: Context, value: T) => void) => void;
 type ItParamTestFuncAsync = <T>(desc: string, data: T[], callback: (this: Context, done: Done, value: T) => void) => void;
 type ItParamTestFunc = ItParamTestFuncSync & ItParamTestFuncAsync;
 
-declare const itParam: ItParamTestFunc & {
-	skip: ItParamTestFunc;
-	only: ItParamTestFunc;
-	retries: ItParamTestFunc;
-}
+type Utilities = { [P in keyof TestFunction]: ItParamTestFunc };
 
+declare const itParam: ItParamTestFunc & Utilities;
 export default itParam;
