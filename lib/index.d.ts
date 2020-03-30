66ac1b8a9beaa76ad26ddc54fb85b6b810cce26f
HoldYourWaffle <ravivanrooijen@live.nl>
2020-03-30 14:13:11 +0200

    Update type definitions with utility functions (manual)

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,4 +1,14 @@
 import { Context, Done } from 'mocha'
 
-export default function itParam<T>(desc: string, data: T[], callback: (this: Context, value: T) => void): void;
-export default function itParam<T>(desc: string, data: T[], callback: (this: Context, done: Done, value: T) => void): void;
+
+type ItParamTestFuncSync = <T>(desc: string, data: T[], callback: (this: Context, value: T) => void) => void;
+type ItParamTestFuncAsync = <T>(desc: string, data: T[], callback: (this: Context, done: Done, value: T) => void) => void;
+type ItParamTestFunc = ItParamTestFuncSync & ItParamTestFuncAsync;
+
+declare const itParam: ItParamTestFunc & {
+	skip: ItParamTestFunc;
+	only: ItParamTestFunc;
+	retries: ItParamTestFunc;
+}
+
+export default itParam;
